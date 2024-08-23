b776dd0a801db68a82019c85b01f1fb544ee83ad
getsetgo1 <dnwls1313@gmail.com>
2024-08-23 12:26:19 +0900

    자동화

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -11,7 +11,7 @@
           </div>
         </v-col>
         <v-col class="text-center">
-          <v-btn :to="{ path: '/' }">java shop22222</v-btn>
+          <v-btn :to="{ path: '/' }">java shop33333</v-btn>
         </v-col>
 
         <!-- d-flex justify-end : 오른쪽 정렬 -->
