7476c3ff0be279a4f5b94d5cdc2a80404323fcd3
getsetgo1 <dnwls1313@gmail.com>
2024-08-23 14:05:24 +0900

    dev로 push

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -11,7 +11,7 @@
           </div>
         </v-col>
         <v-col class="text-center">
-          <v-btn :to="{ path: '/' }">java shop33333</v-btn>
+          <v-btn :to="{ path: '/' }">java shop4444</v-btn>
         </v-col>
 
         <!-- d-flex justify-end : 오른쪽 정렬 -->
